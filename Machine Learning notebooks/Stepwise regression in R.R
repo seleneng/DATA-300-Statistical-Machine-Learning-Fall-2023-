f4af869de4a3b708acf44070886523c377bfa203
Kennedy Odongo <54551893+KennedyOdongo@users.noreply.github.com>
2023-10-25 11:36:00 -0400

    adding LASSO papers and R code for Stepwise regression

---
+++ Machine Learning notebooks/Stepwise regression in R.R
@@ -0,0 +1,23 @@
+library(MASS)
+
+df<-read.csv("C:/Users/rodge/Downloads/California_Houses.csv")
+ls(df)
+
+fullmodel<-lm(Median_House_Value~., data =df)
+fullmodel
+
+intmodel<-lm(Median_House_Value~1, data=df)
+
+
+summary(stepAIC(intmodel, # start with a model containing no variables
+                direction = 'forward', # run forward selection
+                scope = list(upper = fullmodel, # the maximum to consider is a model with all variables
+                             lower = intmodel), # the minimum to consider is a model with no variables
+                trace = 1)) # do not show the step-by-step process of model selection
+
+summary(stepAIC(fullmodel, # start with a model containing all the variables
+                direction = 'backward', # run backward selection
+                scope = list(upper = fullmodel, # the maximum to consider is a model with all variables
+                             lower = intmodel), # the minimum to consider is a model with no variables
+                trace = 0)) # do not show the step-by-step process of model selection
+length(ls(df))
